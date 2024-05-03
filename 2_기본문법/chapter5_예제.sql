2a792fd32fb82ed7b19d2f8db807abed2bfaa0dc
user1 <user1@DESKTOP-THB5PT3>
2024-05-03 17:22:50 +0900

    식별자 유효 범위에 대한 예제 추가

--- 2_기본문법/chapter5_예제.sql
+++ 2_기본문법/chapter5_예제.sql
@@ -42,3 +42,110 @@ begin
     DBMS_OUTPUT.PUT_LINE("BEGIN");
     DBMS_OUTPUT.PUT_LINE("square number"(10));
 end;
+
+--식별자명 충돌 시의 유효범위
+--아래의 예시는 inner블록의 v_num은 밖에서 인식하지 못한다.
+set serveroutput on;
+declare
+    v_num number := 0;
+begin
+    declare
+        v_num number;
+    begin
+        v_num := 4;
+        DBMS_OUTPUT.PUT_LINE('v_num : ' || v_num);
+    end;
+    v_num := v_num + 1;
+    DBMS_OUTPUT.PUT_LINE('v_num : ' || v_num);
+end;
+
+--레이블을 사용한 식별자 참조
+<<OUTER_BLOCK>>
+declare
+    v_num number := 0;
+begin
+    declare
+        v_num number;
+    begin
+        OUTER_BLOCK.v_num := 4;
+        v_num := 1;
+    end;
+    DBMS_OUTPUT.PUT_LINE('v_num : ' || v_num);
+end;
+
+-- 5-6 서브프로그램에서의 식별자 유효 범위
+create or replace procedure check_salary(a_empno number)
+is
+    v_name varchar2(10);
+    v_num number;
+    
+    function check_bonus(a_empno number) return boolean
+    is
+        v_num number;
+        begin
+            select comm
+            into v_num
+            from emp
+            where empno = a_empno;
+            
+            DBMS_OUTPUT.PUT_LINE(v_name || '의 커미션: ' || v_num);
+        
+            if check_salary.v_num < v_num then
+                return false;
+            else
+                return true;
+            end if;
+        end;
+
+begin
+    select ename, sal
+    into v_name, v_num
+    from emp
+    where empno = a_empno;
+    
+    if check_bonus(a_empno) then
+        DBMS_OUTPUT.PUT_LINE('사원 ' || v_name || '의 커미션이 과도합니다');
+    else
+        DBMS_OUTPUT.PUT_LINE('사원 ' || v_name || '의 커미션이 정상입니다');
+    end if;
+end;
+
+--check_salary 프로시저 실행
+set serveroutput on;
+begin
+    check_salary(7369);
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
